43abc6d11eadb7fc9331e26c8239c0aed0522e3f
Jesse-Driskill <jesse@driskill.com>
2023-09-29 23:23:28 -0700

    Adds .env variables

--- myApp.js
+++ myApp.js
@@ -1,7 +1,16 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
 
 console.log("Hello World");
+
+app.get("/json", (req, res) => {
+    let message = "Hello json";
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        message = message.toUpperCase();
+    }
+    res.json({message: message});
+});
 
 app.use("/public", express.static(__dirname + "/public"));
 
